755f1cb4ca682b1fafe2c1cd9e0302d45a4fcc49
JoseAngelCasianiEcheverria <joseangelcasianiecheverria@gmail.com>
2025-02-24 19:00:01 -0500

    Ejercicio 6 Taller 8

---
+++ Ejercicio6_T8/src/main/java/Error/Ejercicio6_T8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Error;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Ejercicio6_T8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
